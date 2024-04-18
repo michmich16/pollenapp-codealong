4ea2ab35aa56c5d81c0d16381dfee7f2afaea381
Michael P <102651473+michmich16@users.noreply.github.com>
2024-04-18 13:52:53 +0200

    gvre

--- assets/js/components/myPollList.js
+++ assets/js/components/myPollList.js
@@ -1,6 +1,10 @@
 import { myFetch } from "../Utils/apiUtils.js";
 
-export const myPollList = () => {
-  const endpoint ="https://air-quality-api.open-meteo.com/v1/air-quality?latitude=52.52&longitude=13.41&current=alder_pollen,birch_pollen,grass_pollen,mugwort_pollen,olive_pollen,ragweed_pollen&hourly=pm10,pm2_5";
+export const myPollList = async () => {
+  const endpoint = 'https://air-quality-api.open-meteo.com/v1/air-quality?latitude=52.52&longitude=13.41&current=alder_pollen,birch_pollen,grass_pollen,mugwort_pollen,olive_pollen,ragweed_pollen&hourly=pm10,pm2_5';
   const result = await myFetch(endpoint)
+  
+  const { current } = result
+
+  console.log(current);
 };
